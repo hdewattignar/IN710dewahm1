c09ace00de762ca3da8dde45492c1d37c9b3d5fe
hdewattignar <hdewattignar@gmail.com>
2016-03-07 13:44:17 +1300

    string stack first commit
    basic setup of classes

---
+++ StringStack/StringStack/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringStack
+{
+    class Stack
+    {
+
+        public Stack()
+        {
+ 
+        }
+
+        public void Push(string newString)
+        {
+ 
+        }
+
+        public String Pop()
+        {
+ 
+        }
+
+        public String Peek()
+        {
+ 
+        }
+
+        public int Count()
+        {
+ 
+        }
+
+        public bool IsEmpty()
+        {
+ 
+        }
+    }
+}
