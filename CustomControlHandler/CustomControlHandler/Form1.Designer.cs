31a5fc707e48358153c7820fdcb9f8219c57ff67
hdewattignar <hdewattignar@gmail.com>
2016-04-07 10:48:01 +1200

    Finished

--- CustomControlHandler/CustomControlHandler/Form1.Designer.cs
+++ CustomControlHandler/CustomControlHandler/Form1.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_testHandlers = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_testHandlers
+            // 
+            this.btn_testHandlers.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_testHandlers.Location = new System.Drawing.Point(13, 13);
+            this.btn_testHandlers.Name = "btn_testHandlers";
+            this.btn_testHandlers.Size = new System.Drawing.Size(259, 115);
+            this.btn_testHandlers.TabIndex = 0;
+            this.btn_testHandlers.Text = "Test Handlers";
+            this.btn_testHandlers.UseVisualStyleBackColor = true;
+            this.btn_testHandlers.Click += new System.EventHandler(this.btn_testHandlers_Click);
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 140);
+            this.Controls.Add(this.btn_testHandlers);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_testHandlers;
     }
 }
 
--- CustomControlHandler/CustomControlHandler/Form1.cs
+++ CustomControlHandler/CustomControlHandler/Form1.cs
@@ -12,9 +12,23 @@ namespace CustomControlHandler
 {
     public partial class Form1 : Form
     {
+        public delegate void ClickEventHandler(object sender, EventArgs e);
         public Form1()
         {
             InitializeComponent();
+
+            CustomHandler handler1 = new CustomHandler();
+
+            this.btn_testHandlers.Click += new System.EventHandler(handler1.CustomClickHandler);
+
+            CustomHandler handler2 = new CustomHandler();
+
+            this.btn_testHandlers.Click += new System.EventHandler(handler2.CustomClickHandler);
+        }
+
+        private void btn_testHandlers_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is the forms button handler");
         }
     }
 }
