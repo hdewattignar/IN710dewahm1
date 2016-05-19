dd2443c877bb1cd2d2eee08b884e4ff9222c096f
hdewattignar <hdewattignar@gmail.com>
2016-05-19 12:38:52 +1200

    MVC

--- firstMVC/firstMVC/Controllers/WelcomeController.cs
+++ firstMVC/firstMVC/Controllers/WelcomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using firstMVC.Models;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +11,27 @@ namespace firstMVC.Controllers
     public class WelcomeController : Controller
     {
         // GET: Welcome
+
+        public Holiday CurrentHoliday {get;set;}
+        
+
         public ActionResult Index()
         {
-            return View();
+            Holiday christmas = new Holiday("christmas", new DateTime(2015, 12, 25), "christmas.jpg");
+            Holiday easter = new Holiday("easter", new DateTime(2015, 04, 16), "easter.jpg");
+            Holiday halloween = new Holiday("halloween", new DateTime(2015, 10, 31), "halloween.jpg");
+
+            Holiday[] holidays = new Holiday[] { christmas, easter, halloween };
+            Random rnd = new Random();
+            CurrentHoliday = holidays[rnd.Next(3)];
+            return View(CurrentHoliday);
+        }
+        
+        public string getDaysUntill()
+        {
+            DateTime currentDate = DateTime.Now;
+            TimeSpan daysTil = CurrentHoliday.Date - currentDate;
+            return Convert.ToString(daysTil.Days);
         }
     }
 }
