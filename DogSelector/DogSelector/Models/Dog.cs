13c6e962f9a6806d3ed9ebd25b7411183b79bd05
hdewattignar <hdewattignar@gmail.com>
2016-05-23 13:35:43 +1200

    working

--- DogSelector/DogSelector/Models/Dog.cs
+++ DogSelector/DogSelector/Models/Dog.cs
@@ -7,19 +7,19 @@ namespace DogSelector.Models
 {
     public enum EScale { High, Medium, Low};
     public enum ELength { Long, Medium, Short};
-    public enum ESize { Large, Medium, Small};
+    public enum ESize { Giant, Large, Medium, Small, Miniature};
     public class Dog
     {
         public string BreedName { get; set; }
         public string DisplayName { get; set; }
         public string ImageName { get; set; }
         public EScale ActivityLevel { get; set; }
-        public EScale SheddingLavel { get; set; }
+        public EScale SheddingLevel { get; set; }
         public EScale GroomingLevel { get; set; }
         public EScale IntelligenceLevel { get; set; }
         public bool GoodWithChildren { get; set; }
         public bool Drools { get; set; }
-        public ELength CoatLength { get; set; }
-        public ESize size { get; set; }
+        public ELength Coatlength { get; set; }
+        public ESize Size { get; set; }
     }
 }
